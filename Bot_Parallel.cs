66e5755a466867fcfb779c385f8c788ea27943b9
timurfromcanada <timurfromcanada@gmail.com>
2021-04-13 10:36:51 +0500

    Implemented Bot_Parallel.cs

--- Bot_Parallel.cs
+++ Bot_Parallel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +10,23 @@ namespace rocket_bot
     {
         public Rocket GetNextMove(Rocket rocket)
         {
-            // TODO: распараллелить запуск SearchBestMove
-            var bestMove = SearchBestMove(rocket, new Random(random.Next()), iterationsCount);
-            var newRocket = rocket.Move(bestMove.Item1, level);
-            return newRocket;
+            var moves = new ConcurrentBag<Tuple<Turn, double>>();
+            var tasks = new Task[threadsCount];
+
+            for (var i = 0; i < threadsCount; i++)
+            {
+                tasks[i] = new Task(() =>
+                {
+                    var random = new Random();
+                    var bestMove = SearchBestMove(rocket, this.random, iterationsCount / threadsCount);
+                    moves.Add(bestMove);
+                });
+                tasks[i].Start();
+            }
+
+            Task.WaitAll(tasks);
+            var result = moves.OrderBy(w => w.Item2).First();
+            return rocket.Move(result.Item1, level);
         }
     }
 }
